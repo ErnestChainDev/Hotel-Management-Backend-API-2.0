36f550a41e566bcefa033749cf50fc84d4db0736
Ernest Windel Dreo <ewdreo@sorsu.edu.ph>
2025-12-01 23:46:53 +0800

    Enable credentials in CORS middleware

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const app = express();
 // CORS Middleware
 app.use(cors({
   origin: 'http://localhost:3000',
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
